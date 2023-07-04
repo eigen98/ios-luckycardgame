a08f56b7e651f851111aaaf5f4f49db81b478e1a
eigen98 <68258365+eigen98@users.noreply.github.com>
2023-07-04 18:37:36 +0900

    [고정민] - 1주차 게임보드 만들기 (#18)
    * Feat : (UIColor Extenstion) 16진수 색상코드 변환 코드 추가.
    
    * Feat : (GameBoard) 게임 보드 만들기 UI 구현
    1. 주어진 레이아웃에 따라 뷰의 크기와 위치를 설정하는 layout 함수 구현
    2. 각 뷰와 라벨의 속성을 설정하는 attribute 함수 구현
    
    * UserInterfaceState.xcuserstate
    
    * Removed file that shouldnt be tracked
    
    * Remove userState
    
    * Removed file that shouldnt be tracked
    
    * Feat : Plist를 통해 앱 이름 변경하기
    
    ---------
    
    Co-authored-by: KoJeongMin <kojeongmin@KoJeongMinui-MacBookPro.local>

---
+++ LuckyCardGame/LuckyCardGame/Common/Colors.swift
@@ -0,0 +1,12 @@
+//
+//  Colors.swift
+//  LuckyCardGame
+//
+//  Created by KoJeongMin  on 2023/07/04.
+//
+
+import Foundation
+import UIKit
+final class Colors{
+    static let player_container_colors = UIColor.init(hex: "#e5e5e5")
+}
