75036383cd19fe09383614a9c4f5df2393d224f0
eigen98 <terrantrace@gmail.com>
2023-07-13 17:26:06 +0900

    Feat : (UnitTest) 테스트코드 과제에 필요한 기능 추가

--- LuckyCardGame/LuckyCardGame/Entity/GameManager.swift
+++ LuckyCardGame/LuckyCardGame/Entity/GameManager.swift
@@ -9,8 +9,6 @@ import Foundation
 //게임 현황
 struct GameManager{
     
-    
-  
     private var players: [Player]
     private var bottomDeck: [LuckyCard]
     private var dealer: Dealer
@@ -31,9 +29,12 @@ struct GameManager{
         for playerIndex in players.indices {
             let cards = dealer.deal(count: cardsPerPlayer)
             players[playerIndex].playerDeck.append(contentsOf: cards)
+            sortPlayerDeck(at: playerIndex)
         }
-
+        
+        
         bottomDeck.append(contentsOf: dealer.deal(count: dealer.count()))
+        sortBottomDeck()
     }
     
     //플레이어 입장
@@ -70,10 +71,73 @@ struct GameManager{
     func getMode() -> GameMode{
         return mode
     }
+
     
-    mutating func setMode(mode: GameMode){
-        self.mode = mode
+    /*
+     레이어의 카드 덱을 오름차순으로 정렬하는 함수
+     index를 이용하여 해당 플레이어를 선택하고, 해당 플레이어의 카드 덱을 정렬
+    */
+    mutating func sortPlayerDeck(at index: Int) {
+        guard players.indices.contains(index) else {
+            return
+        }
+        players[index].playerDeck.sort(by: { $0.number.rawValue < $1.number.rawValue })
     }
+
+    mutating func sortBottomDeck() {
+        bottomDeck.sort(by: { $0.number.rawValue < $1.number.rawValue })
+    }
+    
+    /*
+     참가자 중에 같은 숫자 카드 3장을 가진 경우가 있는지 판단하는 함수
+     카드들의 숫자별 개수를 세는 딕셔너리 생성하여 3개 이상의 카드를 가진 숫자가 있는지 확인
+    */
+    func hasTripleCardsInPlayers(player : Player) -> Bool {
+        
+        let counts = player.playerDeck.reduce(into: [CardNumber: Int]()) { counts, card in
+            counts[card.number, default: 0] += 1
+        }
+
+        for count in counts.values {
+            if count >= 3 {
+                return true
+            }
+        }
+        return false
+    }
+    
+    
+    
+    /*
+     플레이어의 (가장 높은, 가장 낮은)카드 선택
+     */
+    func selectCardFromPlayer(player : Player,  isHighest : Bool) -> Card?{
+        let cardCount = player.playerDeck.count
+        if cardCount == 0{
+            return nil
+        }
+        if isHighest{
+            return player.playerDeck[cardCount - 1]
+        }else{
+            return player.playerDeck[0]
+        }
+    }
+    
+    /*
+     바닥 (가장 높은, 가장 낮은)카드 선택
+     */
+    func selectCardFromBottom(bottomDeck : [Card], isHighest : Bool) -> Card?{
+        let cardCount = bottomDeck.count
+        if cardCount == 0{
+            return nil
+        }
+        if isHighest{
+            return bottomDeck[cardCount - 1]
+        }else{
+            return bottomDeck[0]
+        }
+    }
+    
     
     
 }
